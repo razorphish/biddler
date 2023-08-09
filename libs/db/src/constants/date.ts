5dc006ac473a63e4c9936aeb4baa84176710879b
Antonio Marasco <Antonio.Marasco@softrams.com>
2023-08-09 16:41:18 -0700

    fixed error

--- libs/db/src/constants/date.ts
+++ libs/db/src/constants/date.ts
@@ -1,7 +1,4 @@
-import { Duration } from 'luxon';
-
 export const GLOBAL_TIME_ZONE = 'America/New_York';
-export const DAY_OFFSET = Duration.fromObject({ days: 1 });
 export const WEEKEND_DAYS = [6, 7];
 
 export const DATETIME_FORMAT = 'M/d/yyyy, h:mm a ZZZZ';
