1bea5dba780591316765a65cf1003099f1f7de8f
Antonio Marasco <Antonio.Marasco@softrams.com>
2023-08-11 10:55:15 -0700

    updated code

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -1,28 +1,28 @@
 module.exports = {
   onPreBuild: ({ utils }) => {
-    const currentProject = process.env.PROJECT_NAME;
+    const currentSite = process.env.SITE_NAME;
     const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
     const latestCommit = 'HEAD';
     const projectHasChanged = projectChanged(
-      currentProject,
+      currentSite,
       lastDeployedCommit,
       latestCommit
     );
     if (!projectHasChanged) {
       utils.build.cancelBuild(
-        `Build was cancelled because ${currentProject} was not affected by the latest changes ${JSON.stringify(process.env)}`
+        `Build was cancelled because ${currentSite} was not affected by the latest changes ${JSON.stringify(process.env)}`
       );
     }
   }
 };
 
-function projectChanged(currentProject, fromHash, toHash) {
+function projectChanged(currentSite, fromHash, toHash) {
   const execSync = require('child_process').execSync;
   const getAffected = `yarn --silent nx print-affected --base=${fromHash} --head=${toHash}`;
   const output = execSync(getAffected).toString();
   //get the list of changed projects from the output
   const changedProjects = JSON.parse(output).projects;
-  if (changedProjects.find(project => project === currentProject)) {
+  if (changedProjects.find(project => project === currentSite)) {
     return true;
   } else {
     return false;
